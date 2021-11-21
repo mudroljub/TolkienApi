1a60b1e58fa4568646a130da116487407df831eb
Damjan Pavlica <damjan.pavlica@origo.is>
2021-11-21 23:12:57 +0100

    CharacterService

--- Services/CharacterService.cs
+++ Services/CharacterService.cs
@@ -1,39 +1,58 @@
+using TolkienApi.Helpers;
 using TolkienApi.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace TolkienApi.Services
 {
     public class CharacterService
     {
+        private readonly DataContext _context;
         private readonly QuoteService _quoteService;
-        readonly Dictionary<string, Character> Characters = new();
 
-        public CharacterService(QuoteService quoteService)
+        public CharacterService(DataContext context, QuoteService quoteService)
         {
+            _context = context;
             _quoteService = quoteService;
-            // foreach (Quote q in _quoteService.GetAll())
-            // {
-            //     Characters.Add(q.Character, new Character()
-            //     {
-            //         Name = q.Character,
-            //         LotrUrl = $"- http://lotr.wikia.com/?curid={q.Lotr_page_id}",
-            //     });  
-            // }
         }
 
-        public Dictionary<string, Character> GetCharacters() => Characters;
+        public IEnumerable<Character> GetAll() => _context.Characters;
 
-        public Character GetCharacterDetails(string author)
-        {
-            IEnumerable<Quote> authorQuotes = _quoteService.GetByAuthor(author);
-            Character authorDetails = new Character()
+        public Character GetById(int id) {
+            var character = _context.Characters.FirstOrDefault(p => p.Id == id);
+            if (character != null)
             {
-                Name = author,
-                // LotrUrl = $"- http://lotr.wikia.com/?curid={q.Lotr_page_id}",
-                Quotes = authorQuotes
-            };
-            return authorDetails;
+                character.Quotes = _quoteService.GetByAuthor(character.Name);
+                character.LotrUrl = $"- http://lotr.wikia.com/?curid={character.Lotr_page_id}";
+            }
+            return character;
+        }
+
+        public Character GetRandom() => _context.Characters.ToList()[new Random().Next(0, _context.Characters.Count())];
+
+        public void Add(Character quote)
+        {
+            _context.Characters.Add(quote);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Character quote)
+        {
+            _context.Characters.Remove(quote);
+            _context.SaveChanges();
+        }
+
+        public void Replace(Character oldCharacter, Character newCharacter)
+        { 
+            _context.Entry(oldCharacter).CurrentValues.SetValues(newCharacter);
+            _context.SaveChanges();
+        }
+
+        public void Update(Character quote)
+        {
+            _context.Characters.Update(quote);
+            _context.SaveChanges();
         }
 
     }
